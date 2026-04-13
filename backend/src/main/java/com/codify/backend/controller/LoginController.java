a7a28ab56dea9dedc7b6f87c56b309d9c1986022
KateSebring <ihatesuperman99@gmail.com>
2026-04-13 16:54:29 -0400

    Add ResponseEntity and DTOs for each mapping

--- backend/src/main/java/com/codify/backend/controller/LoginController.java
+++ backend/src/main/java/com/codify/backend/controller/LoginController.java
@@ -1,13 +1,15 @@
 package com.codify.backend.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.codify.backend.dto.LoginResponse;
+import com.codify.backend.dto.LoginRequest;
 
 @RestController
 @RequestMapping("/api/login")
 public class LoginController {
-	// TODO: needs to take in login request and return login response
 	@PostMapping
-	public void loginUser() {
-		
+	public ResponseEntity<LoginResponse> loginUser(LoginRequest request) {
+		return ResponseEntity.ok(null);
 	}
 }
