a1e7efafa12e0d10c82811df0e1feb5790296518
KateSebring <ihatesuperman99@gmail.com>
2026-04-13 16:25:16 -0400

    Create registration DTOs and correct name of RegistrationController

--- backend/src/main/java/com/codify/backend/controller/RegistrationController.java
+++ backend/src/main/java/com/codify/backend/controller/RegistrationController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/register")
-public class RegistrationRequest {
+public class RegistrationController {
 	// TODO: needs to take in registration request and return registration response
 	@PostMapping
 	public void registerUser() {
