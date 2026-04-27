b42179654e6a95c74ad90bc2358ce40df52552c0
KateSebring <ihatesuperman99@gmail.com>
2026-04-27 18:41:14 -0400

    Finish styling the job application form

--- frontend/src/pages/JobAppSubmission.jsx
+++ frontend/src/pages/JobAppSubmission.jsx
@@ -6,23 +6,38 @@ function JobAppSubmission() {
     return(
         <>
             <Header />
-            <main className='job-app-submission'>
-                <h1>Submit an Application</h1>
+            <main>
+                <h1 className='job-submission-header'>Submit an Application</h1>
                 <section className='form-section'>
-                    <form>
-                        <label for='job-title'>Job Title</label>
-                        <input type='text' id='job-title' name='job-title'></input><br></br>
-                        <label for='company'>Company</label>
-                        <input type='text' id='company' name='company'></input><br></br>
+                    <form class='job-app-form'>
+                        <div className='form-option'>
+                            <label for='job-title'>Job Title</label>
+                            <input type='text' id='job-title' name='job-title'></input>
+                        </div>
+
+                        <div className='form-option'>
+                            <label for='company'>Company</label>
+                            <input type='text' id='company' name='company'></input>
+                        </div>
+
+                        <div className='form-option'>
                         <label for='hourly-pay'>Hourly Pay</label>
-                        <input type='text' id='hourly-pay' name='hourly-pay'></input><br></br>
-                        <label for='app-status'>Application Status</label>
-                        <select id='app-status' name='app-status'>
-                            <option value='applied'>Applied</option>
-                            <option value='applied'>Interviewed</option>
-                            <option value='applied'>Offer</option>
-                            <option value='applied'>Rejected</option>
-                        </select>
+                        <input type='text' id='hourly-pay' name='hourly-pay'></input>
+                        </div>
+
+                        <div className='form-option'>
+                            <label for='app-status'>Application Status</label>
+                            <select id='app-status' name='app-status'>
+                                <option value='applied'>Applied</option>
+                                <option value='applied'>Interviewed</option>
+                                <option value='applied'>Offer</option>
+                                <option value='applied'>Rejected</option>
+                            </select>
+                        </div>
+
+                        <div className='form-option'>
+                            <input type='button' className='submit-form' value='Submit'></input>
+                        </div>
                     </form>
                 </section>
             </main>
