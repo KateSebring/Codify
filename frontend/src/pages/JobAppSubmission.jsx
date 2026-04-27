824f223dd8d2e8bfef1aa206dd0e84950584149c
KateSebring <ihatesuperman99@gmail.com>
2026-04-27 18:53:36 -0400

    Modified JobAppSubmission's CSS file to be a module instead

--- frontend/src/pages/JobAppSubmission.jsx
+++ frontend/src/pages/JobAppSubmission.jsx
@@ -1,32 +1,32 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import '../css/JobAppSubmission.css'
+import styles from '../css/JobAppSubmission.module.css'
 
 function JobAppSubmission() {
     return(
         <>
             <Header />
             <main>
-                <h1 className='job-submission-header'>Submit an Application</h1>
-                <section className='form-section'>
-                    <form class='job-app-form'>
-                        <div className='form-option'>
-                            <label for='job-title'>Job Title</label>
+                <h1 className={styles.jobSubmissionHeader}>Submit an Application</h1>
+                <section className={styles.formSection}>
+                    <form className={styles.jobAppForm}>
+                        <div className={styles.formOption}>
+                            <label htmlFor='job-title'>Job Title</label>
                             <input type='text' id='job-title' name='job-title' placeholder='Job title...'></input>
                         </div>
 
-                        <div className='form-option'>
-                            <label for='company'>Company</label>
+                        <div className={styles.formOption}>
+                            <label htmlFor='company'>Company</label>
                             <input type='text' id='company' name='company' placeholder='Company...'></input>
                         </div>
 
-                        <div className='form-option'>
-                        <label for='hourly-pay'>Hourly Pay</label>
+                        <div className={styles.formOption}>
+                        <label htmlFor='hourly-pay'>Hourly Pay</label>
                         <input type='text' id='hourly-pay' name='hourly-pay' placeholder='0.00'></input>
                         </div>
 
-                        <div className='form-option'>
-                            <label for='app-status'>Application Status</label>
+                        <div className={styles.formOption}>
+                            <label htmlFor='app-status'>Application Status</label>
                             <select id='app-status' name='app-status'>
                                 <option value='applied'>Applied</option>
                                 <option value='applied'>Interviewed</option>
@@ -35,8 +35,8 @@ function JobAppSubmission() {
                             </select>
                         </div>
 
-                        <div className='form-option'>
-                            <input type='button' className='submit-form' value='Submit'></input>
+                        <div className={styles.formOption}>
+                            <input type='button' className={styles.submitForm} value='Submit'></input>
                         </div>
                     </form>
                 </section>
