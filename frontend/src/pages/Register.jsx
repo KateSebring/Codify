7036db74f7b1d00c857d5693f6f82dc02af51395
KateSebring <ihatesuperman99@gmail.com>
2026-04-27 19:01:22 -0400

    Get basic layout set up for registration

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,16 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
+import styles from '../css/Register.module.css'
 function Register() {
     return(
         <>
-    <       Header />
-            <h1>Register</h1>
+            <Header />
+            <h1 className={styles.header}>Register</h1>
+            <section className={styles.registerFormSection}>
+                <form className={styles.registrationForm}>
+                    <input type='text'></input>
+                </form>
+            </section>
             <Footer />
         </>
     )
