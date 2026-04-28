901e8cca860d49b3e84b860a51771e631bca3166
KateSebring <ihatesuperman99@gmail.com>
2026-04-28 13:55:47 -0400

    Set up layout for login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,28 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
+import styles from '../css/Login.module.css'
 function Login() {
     return(
         <>
     <       Header />
-            <h1>Login</h1>
+            <h1 className={styles.header}>Login</h1>
+            <section className={styles.loginFormSection}>
+                <form className={styles.loginForm}>
+                    <div className={styles.formOption}>
+                        <label for='username'>Username</label>
+                        <input type='text' id='username' name='username'></input>
+                    </div>
+
+                    <div className={styles.formOption}>
+                        <label for='password'>Password</label>
+                        <input type='password' id='password' name='password'></input>
+                    </div>
+
+                    <div className={styles.formOption}>
+                        <input type='button' className={styles.submitForm} value='Submit'></input>
+                    </div>
+                </form>
+            </section>
             <Footer />
         </>
     )
