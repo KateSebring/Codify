d8d5c9b9ed611c3f63590b064279729a4718b1ec
KateSebring <ihatesuperman99@gmail.com>
2026-05-05 15:02:24 -0400

    Derive if user is logged in from authUser and update Header to match

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,14 @@
 import '../css/Header.css';
 import logo from '../logo.png';
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { API_URL } from '../config'
 
 function Header() {
     const {
-        authUser,
         setAuthUser,
         isLoggedIn,
-        setIsLoggedIn
+        loading
     } = useAuth();
 
     async function handleLogout() {
@@ -21,7 +19,6 @@ function Header() {
             });
 
             setAuthUser(null);
-            setIsLoggedIn(false);
         } catch (err) {
             alert("Something went wrong! " + err);
         }
@@ -32,25 +29,26 @@ function Header() {
             <header>
                 <img src={logo} alt="Codify logo" />
                 <nav>
-                    <ul id="header-items">
-                        <li><Link to="/" className='nav-link'>Home</Link></li>
-                        {!isLoggedIn ? (
-                            <>
-                                <li><Link to="/register" className='nav-link'>Register</Link></li>
-                                <li><Link to="/login" className='nav-link'>Login</Link></li>
-                            </>
-                        ) : (
-                            <>
-                                <li><Link to="/dashboard" className='nav-link'>Dashboard</Link></li>
-                                <li>
-                                    <button onClick={handleLogout}  className='nav-link'>
-                                        Logout
-                                    </button>
-                                </li>
-                            </>
-                        )}
-                        
-                    </ul>
+                    {loading ? null : (
+                        <ul id="header-items">
+                            <li><Link to="/" className='nav-link'>Home</Link></li>
+                            {!isLoggedIn ? (
+                                <>
+                                    <li><Link to="/register" className='nav-link'>Register</Link></li>
+                                    <li><Link to="/login" className='nav-link'>Login</Link></li>
+                                </>
+                            ) : (
+                                <>
+                                    <li><Link to="/dashboard" className='nav-link'>Dashboard</Link></li>
+                                    <li>
+                                        <button onClick={handleLogout}  className='nav-link'>
+                                            Logout
+                                        </button>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    )}
                 </nav>
             </header>
         </>
--- frontend/src/contexts/AuthProvider.jsx
+++ frontend/src/contexts/AuthProvider.jsx
@@ -4,8 +4,9 @@ import { AuthContext } from './AuthContext';
 
 export function AuthProvider({ children }) {
     const [authUser, setAuthUser] = useState(null);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [loading, setLoading] = useState(true);
+
+    const isLoggedIn = !!authUser;
 
     useEffect(() => {
         async function checkAuth() {
@@ -17,14 +18,11 @@ export function AuthProvider({ children }) {
                if(res.ok) {
                 const user = await res.json();
                 setAuthUser(user);
-                setIsLoggedIn(true);
                } else {
                 setAuthUser(null);
-                setIsLoggedIn(false);
                }
             } catch (err) {
                 setAuthUser(null);
-                setIsLoggedIn(false);
             } finally {
                 setLoading(false);
             }
@@ -37,7 +35,7 @@ export function AuthProvider({ children }) {
         authUser,
         setAuthUser,
         isLoggedIn,
-        setIsLoggedIn
+        loading
     }
 
     return(
